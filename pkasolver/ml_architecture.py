c7d348cba3578449d7c9f07a6b922dc44a0989d8
hyunjunji <hyunjun.ji@standigm.com>
2023-08-21 02:45:59 +0000

    feat: pyg version independent architectures

--- pkasolver/ml_architecture.py
+++ pkasolver/ml_architecture.py
@@ -1,7 +1,15 @@
+import copy
+from typing import Optional, List, Callable
+
 import torch
 import torch.nn.functional as F
-from torch.nn import Linear, ModuleList, ReLU, Sequential
+from torch import Tensor
+from torch.nn import BatchNorm1d, Linear, ModuleList, ReLU, Sequential
 from torch_geometric.nn import GCNConv, GlobalAttention, NNConv, global_mean_pool
+from torch_geometric.nn.conv import GINConv
+from torch_geometric.nn.models import GAT, AttentiveFP
+from torch_geometric.nn.models.jumping_knowledge import JumpingKnowledge
+from torch_geometric.typing import Adj
 from tqdm import tqdm
 
 from pkasolver.constants import DEVICE, SEED
@@ -52,7 +60,136 @@ def forward_lins(x, l: list):
 # defining GCN for single state
 #####################################
 #####################################
-from torch_geometric.nn.models import GAT, GIN, AttentiveFP
+
+
+class BasicGNN(torch.nn.Module):
+    r"""An abstract class for implementing basic GNN models.
+
+    Args:
+        in_channels (int): Size of each input sample.
+        hidden_channels (int): Size of each hidden sample.
+        num_layers (int): Number of message passing layers.
+        out_channels (int, optional): If not set to :obj:`None`, will apply a
+            final linear transformation to convert hidden node embeddings to
+            output size :obj:`out_channels`. (default: :obj:`None`)
+        dropout (float, optional): Dropout probability. (default: :obj:`0.`)
+        act (Callable, optional): The non-linear activation function to use.
+            (default: :meth:`torch.nn.ReLU(inplace=True)`)
+        norm (torch.nn.Module, optional): The normalization operator to use.
+            (default: :obj:`None`)
+        jk (str, optional): The Jumping Knowledge mode
+            (:obj:`"last"`, :obj:`"cat"`, :obj:`"max"`, :obj:`"last"`).
+            (default: :obj:`"last"`)
+    """
+    def __init__(self, in_channels: int, hidden_channels: int, num_layers: int,
+                 out_channels: Optional[int] = None, dropout: float = 0.0,
+                 act: Optional[Callable] = ReLU(inplace=True),
+                 norm: Optional[torch.nn.Module] = None, jk: str = 'last'):
+        super().__init__()
+        self.in_channels = in_channels
+        self.hidden_channels = hidden_channels
+        self.num_layers = num_layers
+        self.dropout = dropout
+        self.act = act
+
+        self.convs = ModuleList()
+
+        self.norms = None
+        if norm is not None:
+            self.norms = ModuleList(
+                [copy.deepcopy(norm) for _ in range(num_layers)])
+
+        if jk != 'last':
+            self.jk = JumpingKnowledge(jk, hidden_channels, num_layers)
+
+        if out_channels is not None:
+            self.out_channels = out_channels
+            if jk == 'cat':
+                self.lin = Linear(num_layers * hidden_channels, out_channels)
+            else:
+                self.lin = Linear(hidden_channels, out_channels)
+        else:
+            if jk == 'cat':
+                self.out_channels = num_layers * hidden_channels
+            else:
+                self.out_channels = hidden_channels
+
+    def reset_parameters(self):
+        for conv in self.convs:
+            conv.reset_parameters()
+        for norm in self.norms or []:
+            norm.reset_parameters()
+        if hasattr(self, 'jk'):
+            self.jk.reset_parameters()
+        if hasattr(self, 'lin'):
+            self.lin.reset_parameters()
+
+    def forward(self, x: Tensor, edge_index: Adj, *args, **kwargs) -> Tensor:
+        xs: List[Tensor] = []
+        for i in range(self.num_layers):
+            x = self.convs[i](x, edge_index, *args, **kwargs)
+            if self.norms is not None:
+                x = self.norms[i](x)
+            if self.act is not None:
+                x = self.act(x)
+            x = F.dropout(x, p=self.dropout, training=self.training)
+            if hasattr(self, 'jk'):
+                xs.append(x)
+
+        x = self.jk(xs) if hasattr(self, 'jk') else x
+        x = self.lin(x) if hasattr(self, 'lin') else x
+        return x
+
+    def __repr__(self) -> str:
+        return (f'{self.__class__.__name__}({self.in_channels}, '
+                f'{self.out_channels}, num_layers={self.num_layers})')
+
+
+class GIN(BasicGNN):
+    r"""The Graph Neural Network from the `"How Powerful are Graph Neural
+    Networks?" <https://arxiv.org/abs/1810.00826>`_ paper, using the
+    :class:`~torch_geometric.nn.GINConv` operator for message passing.
+
+    Args:
+        in_channels (int): Size of each input sample.
+        hidden_channels (int): Size of each hidden sample.
+        num_layers (int): Number of message passing layers.
+        out_channels (int, optional): If not set to :obj:`None`, will apply a
+            final linear transformation to convert hidden node embeddings to
+            output size :obj:`out_channels`. (default: :obj:`None`)
+        dropout (float, optional): Dropout probability. (default: :obj:`0.`)
+        act (Callable, optional): The non-linear activation function to use.
+            (default: :meth:`torch.nn.ReLU(inplace=True)`)
+        norm (torch.nn.Module, optional): The normalization operator to use.
+            (default: :obj:`None`)
+        jk (str, optional): The Jumping Knowledge mode
+            (:obj:`"last"`, :obj:`"cat"`, :obj:`"max"`, :obj:`"last"`).
+            (default: :obj:`"last"`)
+        **kwargs (optional): Additional arguments of
+            :class:`torch_geometric.nn.conv.GINConv`.
+    """
+    def __init__(self, in_channels: int, hidden_channels: int, num_layers: int,
+                 out_channels: Optional[int] = None, dropout: float = 0.0,
+                 act: Optional[Callable] = ReLU(inplace=True),
+                 norm: Optional[torch.nn.Module] = None, jk: str = 'last',
+                 **kwargs):
+        super().__init__(in_channels, hidden_channels, num_layers,
+                         out_channels, dropout, act, norm, jk)
+
+        self.convs.append(
+            GINConv(GIN.MLP(in_channels, hidden_channels), **kwargs))
+        for _ in range(1, num_layers):
+            self.convs.append(
+                GINConv(GIN.MLP(hidden_channels, hidden_channels), **kwargs))
+
+    @staticmethod
+    def MLP(in_channels: int, out_channels: int) -> torch.nn.Module:
+        return Sequential(
+            Linear(in_channels, out_channels),
+            BatchNorm1d(out_channels),
+            ReLU(inplace=True),
+            Linear(out_channels, out_channels),
+        )
 
 
 class AttentivePka(AttentiveFP):
